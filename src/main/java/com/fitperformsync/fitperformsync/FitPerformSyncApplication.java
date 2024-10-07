96f1aa4e40d354fad7fc3c18feefb0b777a3d09b
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-10-07 09:40:17 -0300

    first commit

---
+++ src/main/java/com/fitperformsync/fitperformsync/FitPerformSyncApplication.java
@@ -0,0 +1,13 @@
+package com.fitperformsync.fitperformsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPerformSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitPerformSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitperformsync/fitperformsync/FitPerformSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitperformsync.fitperformsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitPerformSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
